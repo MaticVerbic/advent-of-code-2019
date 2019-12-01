d0350edf4ccc1329f571f1f6cda17c0699ca2128
MaticVerbic <matic.verbicit@gmail.com>
2019-12-01 22:42:43 +0100

    Added Day 1 solutions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# advent-of-code-2019-
+# Advent of code
+
+My solutions to advent of code 2019. 
+
+[Advent of code](https://adventofcode.com/2019)
+
---
+++ day_1/run.py
@@ -0,0 +1,42 @@
+'''
+@author: Matic Verbic
+
+Solution for https://adventofcode.com/2019/day/1
+'''
+
+from math import floor
+
+# Reads data from the provided input file. 
+# Returns a list of ints (does no validation). 
+def getData():
+    with open("input.txt") as file:
+        return [int(line) for line in file]
+
+# Simple mass calculation. 
+def calculate_mass(x):
+    return x // 3 - 2
+        
+# Calculates inital mass for each module. 
+def calculate_initial_mass(modules):
+    return sum([calculate_mass(module) for module in modules])
+
+# Calcualtes mass for each modeule recursively. 
+def calculate_mass_recursively(current_mass):
+    mass = calculate_mass(current_mass)
+    if mass < 0:
+        return 0 
+    return mass + calculate_mass_recursively(mass)  
+
+# Solves the first task for day one of advent of code. 
+def task_1():
+    return calculate_initial_mass(getData())
+
+# Solves the second task for day one of advent of code. 
+def task_2():
+    return sum([calculate_mass_recursively(mass) for mass in getData()])
+
+if __name__ == "__main__":
+    print("(task1): Initial mass requires \"{}\" fuel. ".format(task_1()))
+    print("(task2): Recursive mass requires \"{}\" fuel. ".format(task_2()))
+
+
