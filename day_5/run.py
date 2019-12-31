9c24ded5a6e99846c2eb6165c6014d25c40412d6
MaticVerbic <matic.verbicit@gmail.com>
2019-12-31 20:53:20 +0100

    Fixed comments

--- day_5/run.py
+++ day_5/run.py
@@ -22,39 +22,6 @@ ERR_MODES = "invalid mode value at index \"{}\""
 
 SET_OUTPUT_LEVEL="failed"
 
-def init_logger(): 
-    logger = logging.getLogger(__name__)
-    syslog = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s: %(message)s')
-    syslog.setFormatter(formatter)
-    logger.setLevel(logging.ERROR) # change this to info to renable logging (ik hack)
-    logger.addHandler(syslog)
-
-    return logging.LoggerAdapter(logger, {})
-
-logger = init_logger()
-logger.disabled = True
-def opLog(mode, op, opCode, operandOne=None, operandTwo=None, resultIndex=None):
-    if mode == 1:
-        logger.info({
-            "operation": op,
-            "opCode": opCode,
-            "operandOne": operandOne,
-        })
-    if mode == 0: 
-        logger.info({
-            "operation": op,
-            "opCode": opCode,
-            "operandOne": operandOne,
-            "operandTwo": operandTwo,
-            "resultIndex": resultIndex,
-        })
-    if mode == 99:
-        logger.info({
-            "operation": op,
-            "opCode": opCode,
-        })
-
 # Reads data from the provided input file. 
 def getData():
     with open("day_5/input.txt") as file:
@@ -71,14 +38,16 @@ def handler_math(opCode, operandOne, operandTwo):
     if opCode == OP_MUL:
         return operandOne * operandTwo 
     
-
+# Handles input/output operations. 
 def handler_io(opCode, position, data):
+    # Handle writes. 
     if opCode == OP_W:
         try: 
             data[position] = int(input("Input (expected input here is 1): "))
         except Exception as e:
             raise Exception(ERR_INPUT.format(inp))
 
+    # Handle reads. 
     if opCode == OP_R: 
         try: 
             if data[position] != 0: 
@@ -86,7 +55,7 @@ def handler_io(opCode, position, data):
         except Exception as e:
             raise Exception(ERR_OUTPUT)
             
-
+# Parses new format of operation codes. 
 def handler_opcode(opcode):
     # Validate lambda. 
     validate = lambda x: True if x in (0, 1) else False
@@ -96,12 +65,14 @@ def handler_opcode(opcode):
     
     # Generate other commands, valid responses: 1, 0; invalid response: -1. 
     modes = [int(x) if validate(int(x)) else -1 for x in str(opcode)[-3::-1]]
+
+    # If invalid values are found throw an exception. 
     if -1 in modes: 
         raise Exception(ERR_MODES.format(1 + modes.index(-1)))
+        
     # Return [opcode, mode param 1, mode param 2, mode param 3]. 
     return [sum(op)] + modes + [0 for _ in range(4 - (1 + len(modes)))]
     
-
 # Parses and returns an instruction set.
 def parse(data, noun=12, verb=2, mode=0):
     commands = data.copy()
@@ -113,11 +84,13 @@ def parse(data, noun=12, verb=2, mode=0):
     while i < len(commands): 
         # Get opcode. 
         opCode = handler_opcode(commands[i])
+
         # Check for exit condition. 
         if opCode[0] == OP_END: 
             opLog(99, "END", opCode)
             return commands
         
+        # Handle I/O. 
         if opCode[0] in [OP_R, OP_W]: 
             addr = commands[i+1]
             opLog(99, "READ" if opCode[0] == OP_R else "WRITE", opCode, addr)
@@ -126,6 +99,7 @@ def parse(data, noun=12, verb=2, mode=0):
             i += 2 
             continue
 
+        # Handle arithmetic operations. 
         if opCode[0] in [OP_ADD, OP_MUL]:
             resultIndex = commands[i+3] 
 
@@ -140,15 +114,60 @@ def parse(data, noun=12, verb=2, mode=0):
             i += 4
             continue 
 
+        # Invalid opcode was provided, execution error. 
         raise Exception(ERR_OPCODE.format(opCode[0]))
         break
     return commands
 
+# Helper method for logging. 
+def init_logger(): 
+    logger = logging.getLogger(__name__)
+    syslog = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s: %(message)s')
+    syslog.setFormatter(formatter)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(syslog)
+
+    return logging.LoggerAdapter(logger, {})
+
+# Helper method for logging. 
+def opLog(mode, op, opCode, operandOne=None, operandTwo=None, resultIndex=None):
+    # Log IO operations. 
+    if mode == 1:
+        logger.info({
+            "operation": op,
+            "opCode": opCode,
+            "operandOne": operandOne,
+        })
+
+    # Log arithmetic operations. 
+    if mode == 0: 
+        logger.info({
+            "operation": op,
+            "opCode": opCode,
+            "operandOne": operandOne,
+            "operandTwo": operandTwo,
+            "resultIndex": resultIndex,
+        })
+
+    # Log exit. 
+    if mode == 99:
+        logger.info({
+            "operation": op,
+            "opCode": opCode,
+        })
+
 
 if __name__ == "__main__":
     try:
+        # Init and enable the logger. 
+        logger = init_logger()
+        logger.disabled = True
+        
+        # Task one. 
         data = getData() 
         parse(data, mode=1)
+
     except Exception as e:
         traceback.print_exc()
  
