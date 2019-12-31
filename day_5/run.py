a475fa70f788c442c10dd865ff3b680a334b3579
MaticVerbic <matic.verbicit@gmail.com>
2019-12-31 23:41:17 +0100

    Added part 2 of day 5

--- day_5/run.py
+++ day_5/run.py
@@ -9,52 +9,30 @@ import logging
 from itertools import product
 
 # Define constants. 
-OP_END = 99
-OP_ADD = 1
-OP_MUL = 2
-OP_W = 3
-OP_R = 4
+OP_END = 99 # Exit
+OP_ADD = 1  # Addition
+OP_MUL = 2  # Multiplication
+OP_W = 3    # Write to memory
+OP_R = 4    # Read from memory
+OP_JMT = 5  # Jump when true
+OP_JMF = 6  # Jump when false
+OP_LT = 7   # Less than
+OP_EQ = 8   # Equal
+OP_GT = 9   # Greater than
 
 ERR_OPCODE = "failed to parse operand code: \"{}\""
 ERR_INPUT = "failed to parse input: \"{}\""
 ERR_OUTPUT = "failed to print output"
 ERR_MODES = "invalid mode value at index \"{}\""
 
-SET_OUTPUT_LEVEL="failed"
-
 # Reads data from the provided input file. 
-def getData():
+def getData(task):
     with open("day_5/input.txt") as file:
         for  line in file: 
             # Split and convert to int
             return [int(i) for i in line.split(",")]
     return []
 
-# Defines a possible mathematical operation and returns a result. 
-def handler_math(opCode, operandOne, operandTwo):     
-    if opCode == OP_ADD:
-        return operandOne + operandTwo
-
-    if opCode == OP_MUL:
-        return operandOne * operandTwo 
-    
-# Handles input/output operations. 
-def handler_io(opCode, position, data):
-    # Handle writes. 
-    if opCode == OP_W:
-        try: 
-            data[position] = int(input("Input (expected input here is 1): "))
-        except Exception as e:
-            raise Exception(ERR_INPUT.format(inp))
-
-    # Handle reads. 
-    if opCode == OP_R: 
-        try: 
-            if data[position] != 0: 
-                print("(task1): {}".format(data[position]))
-        except Exception as e:
-            raise Exception(ERR_OUTPUT)
-            
 # Parses new format of operation codes. 
 def handler_opcode(opcode):
     # Validate lambda. 
@@ -72,9 +50,58 @@ def handler_opcode(opcode):
 
     # Return [opcode, mode param 1, mode param 2, mode param 3]. 
     return [sum(op)] + modes + [0 for _ in range(4 - (1 + len(modes)))]
+
+# Defines a possible mathematical operation and returns a result. 
+def handler_math(opCode, operandOne, operandTwo):     
+    if opCode == OP_ADD:
+        return operandOne + operandTwo
+
+    if opCode == OP_MUL:
+        return operandOne * operandTwo 
     
+# Handles input/output operations. 
+def handler_io(opCode, position, data, task):
+    # Handle writes. 
+    if opCode == OP_W:
+        try: 
+            data[position] = int(input("Input(expected input is {}): ".format("1" if task == 1 else "5")))
+            return
+        except Exception as e:
+            raise Exception(ERR_INPUT.format(inp))
+
+    # Handle reads. 
+    if opCode == OP_R: 
+        try: 
+            if task == 1 and data[position] == 0: 
+                return    
+            print("(task{}): {}".format(task, data[position]))
+        except Exception as e:
+            raise Exception(ERR_OUTPUT)
+
+# Handles jump operations. 
+def handler_jump(i, opCode, operandOne, operandTwo):
+    return operandTwo if handler_compare(opCode, operandOne) else i + 3
+
+def handler_compare(opCode, operandOne, operandTwo=None):
+    # Handle if true.
+    if opCode[0] == OP_JMT:
+        return bool(operandOne)
+
+    # Handle if false
+    if opCode[0] == OP_JMF:
+        return not bool(operandOne) 
+    
+    if opCode[0] == OP_GT: 
+        return operandOne > operandTwo
+    
+    if opCode[0] == OP_LT: 
+        return operandOne < operandTwo
+    
+    if opCode[0] == OP_EQ:
+        return operandOne == operandTwo
+
 # Parses and returns an instruction set.
-def parse(data, noun=12, verb=2, mode=0):
+def parse(data, noun=12, verb=2, mode=0, task=1):
     commands = data.copy()
     if mode == 0: 
         commands[1] = noun
@@ -90,17 +117,20 @@ def parse(data, noun=12, verb=2, mode=0):
             opLog(99, "END", opCode)
             return commands
         
-        # Handle I/O. 
+        # Handle I/O.  
         if opCode[0] in [OP_R, OP_W]: 
+            mode = "READ" if opCode[0] == OP_R else "WRITE"
             addr = commands[i+1]
-            opLog(99, "READ" if opCode[0] == OP_R else "WRITE", opCode, addr)
-            handler_io(opCode[0], addr, commands)
+
+            opLog(99, mode, opCode, resultIndex=addr)
+            handler_io(opCode[0], addr, commands, task)
 
             i += 2 
             continue
 
         # Handle arithmetic operations. 
         if opCode[0] in [OP_ADD, OP_MUL]:
+            mode = "ADD" if opCode[0] == OP_ADD else "MUL"
             resultIndex = commands[i+3] 
 
             # Get values. 
@@ -108,11 +138,40 @@ def parse(data, noun=12, verb=2, mode=0):
             operandTwo = commands[i+2] if opCode[2] == 1 else commands[commands[i+2]]
             
             # Perform the mathematical operation
-            opLog(0, "ADD" if opCode[0] == OP_ADD else "MUL", opCode, operandOne, operandTwo, resultIndex)
+            opLog(0, mode, opCode, operandOne, operandTwo, resultIndex)
             commands[resultIndex] = handler_math(opCode[0], operandOne, operandTwo)
 
             i += 4
             continue 
+        
+        # Handle jumps. 
+        if opCode[0] in [OP_JMT, OP_JMF]:
+            mode = "JMT" if opCode[0] == OP_JMT else "JMF"
+
+            # Get values. 
+            operandOne = commands[i+1] if opCode[1] == 1 else commands[commands[i+1]]
+            operandTwo = commands[i+2] if opCode[2] == 1 else commands[commands[i+2]]
+
+            # Parse jump. 
+            i = handler_jump(i, opCode, operandOne, operandTwo)
+
+            opLog(2, mode, opCode, operandOne, operandTwo)
+            continue
+        
+        # Handle boolean expressions. 
+        if opCode[0] in [OP_LT, OP_EQ, OP_GT]:
+            resultIndex = commands[i+3] 
+
+            # Get values. 
+            operandOne = commands[i+1] if opCode[1] == 1 else commands[commands[i+1]]
+            operandTwo = commands[i+2] if opCode[2] == 1 else commands[commands[i+2]]
+
+            # Set value
+            opLog(0, mode, opCode, operandOne, operandTwo, resultIndex)
+            commands[resultIndex] = 1 if handler_compare(opCode, operandOne, operandTwo) else 0
+            
+            i += 4
+            continue
 
         # Invalid opcode was provided, execution error. 
         raise Exception(ERR_OPCODE.format(opCode[0]))
@@ -133,15 +192,7 @@ def init_logger():
 
 # Helper method for logging. 
 def opLog(mode, op, opCode, operandOne=None, operandTwo=None, resultIndex=None):
-    # Log IO operations. 
-    if mode == 1:
-        logger.info({
-            "operation": op,
-            "opCode": opCode,
-            "operandOne": operandOne,
-        })
-
-    # Log arithmetic operations. 
+    # Log arithmetic and boolean operations. 
     if mode == 0: 
         logger.info({
             "operation": op,
@@ -151,11 +202,29 @@ def opLog(mode, op, opCode, operandOne=None, operandTwo=None, resultIndex=None):
             "resultIndex": resultIndex,
         })
 
+    # Log IO operations. 
+    if mode == 1:
+        logger.info({
+            "operation": op,
+            "opCode": opCode,
+            "operandOne": operandOne,
+        })
+    
+    # Log jumps. 
+    if mode == 2: 
+        logger.info({
+            "operation": op,
+            "opCode": opCode,
+            "operandOne": operandOne,
+            "operandTwo": operandTwo,
+        })
+
     # Log exit. 
     if mode == 99:
         logger.info({
             "operation": op,
             "opCode": opCode,
+            "addr": resultIndex,
         })
 
 
@@ -163,11 +232,16 @@ if __name__ == "__main__":
     try:
         # Init the logger. 
         logger = init_logger()
+        # logger.info({"status": "init"})
         
         # Task one. 
-        data = getData() 
-        parse(data, mode=1)
+        data = getData(1) 
+        parse(data, mode=1, task=1)
 
+        data= getData(2)
+        parse(data, mode=1, task=2)
+
+        
     except Exception as e:
         traceback.print_exc()
  
